e1cd3632f4db40658d876c9b6a531127cd3bd877
Maksym Shcheglov <maxscheglov@gmail.com>
2019-10-02 17:39:22 +0200

    Initial commit.

---
+++ TMDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TMDB
+//
+//  Created by Maksym Shcheglov on 02/10/2019.
+//  Copyright © 2019 Maksym Shcheglov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
